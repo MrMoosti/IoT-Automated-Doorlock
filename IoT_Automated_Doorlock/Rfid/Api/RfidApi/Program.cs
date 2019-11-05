eb9bb63d41e1c9f2c3857e14dedb7b5bc7b27e98
M.E. Yilmaz <meyilmaz2001@hotmail.com>
2019-11-05 09:05:34 +0100

    Host on ip

--- IoT_Automated_Doorlock/Rfid/Api/RfidApi/Program.cs
+++ IoT_Automated_Doorlock/Rfid/Api/RfidApi/Program.cs
@@ -1,4 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Reflection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
@@ -16,8 +21,21 @@ namespace RfidApi
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var firstUpInterface = NetworkInterface.GetAllNetworkInterfaces()
+                                                   .FirstOrDefault(c => c.NetworkInterfaceType != NetworkInterfaceType.Loopback && c.OperationalStatus == OperationalStatus.Up);
+            if (firstUpInterface == null) throw new Exception("Unable to find Ipv4 address!");
+            var ipv4 = firstUpInterface.GetIPProperties().UnicastAddresses
+                                       .Where(c => c.Address.AddressFamily == AddressFamily.InterNetwork)
+                                       .Select(c => c.Address)
+                                       .FirstOrDefault()
+                                       ?.ToString();
+
+            Console.WriteLine($"Build: {Assembly.GetExecutingAssembly()}, Ip={ipv4}, ProcessId={Process.GetCurrentProcess().Id}");
+            return WebHost.CreateDefaultBuilder(args)
+                .UseUrls($"https://{ipv4}:5001")
                 .UseStartup<Startup>();
+        }
     }
 }
